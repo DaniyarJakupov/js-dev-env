6e4545a34da3f0f96ed8553fcf79d2ed525686df
Daniyar <ddr199305@gmail.com>
2017-01-29 19:41:44 +0100

    Add and configure ESLint (watch mode)

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
 console.log(chalk.blue('Starting app in dev mode...'));
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const value = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${value} for this`);
+debugger; // eslint-disable-line no-debugger
+console.log(`I would pay ${value} for this`); // eslint-disable-line no-console
